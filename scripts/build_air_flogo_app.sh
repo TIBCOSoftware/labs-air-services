2d083e3220199fa12e3ad7a6344bb111f4beedeb
Miguel Torres <mtorres@MTORRES-MBP16.local>
2021-10-11 12:27:14 -0700

    Add gcc to build flogo

--- scripts/build_air_flogo_app.sh
+++ scripts/build_air_flogo_app.sh
@@ -7,16 +7,12 @@ image_tag=$2
 image_url=$3
 app_name=$4
 
-
 local_image_name="flogo/${app_name}"
 local_image_tag="local_image_tag"
 
-
 pushd ./flogo > /dev/null
 
-
 docker build --no-cache --build-arg APP_NAME=${app_name}.json -t ${local_image_name}:${local_image_tag} -f "Dockerfile_flogo_app_builder" .
-
 
 # Tag image
 tag_image $local_image_name $local_image_tag $image_name $image_tag $image_url

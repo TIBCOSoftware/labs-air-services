04b9ac961b53a4213e39c8a23fd099e2f0b6d997
Miguel Torres <mtorres@MTORRES-MBP16.local>
2021-10-11 13:06:52 -0700

    Add the right path to flogo apps

--- scripts/build_air_flogo_builder.sh
+++ scripts/build_air_flogo_builder.sh
@@ -7,7 +7,7 @@ image_tag=$2
 image_url=$3
 flogo_build_type=$4
 
-pushd ./flogo-oss > /dev/null
+pushd ./flogo > /dev/null
 
 build_image $image_name "local_image_tag" $image_url "Dockerfile_flogo_builder_${flogo_build_type}"
 
